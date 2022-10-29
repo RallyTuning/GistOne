cc022be46b4be08639748fac8fcb292084dea082
Gianluigi <50233545+RallyTuning@users.noreply.github.com>
2022-10-30 01:21:18 +0200

    Browse working

--- GistOne/Form1.Designer.cs
+++ GistOne/Form1.Designer.cs
@@ -30,11 +30,27 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader7 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader8 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader9 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader10 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader11 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader12 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader13 = new System.Windows.Forms.ColumnHeader();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(82, 68);
+            this.button1.Location = new System.Drawing.Point(93, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,19 +60,68 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(91, 252);
+            this.button2.Location = new System.Drawing.Point(12, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "serialize";
+            this.button2.Text = "list";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(255, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10,
+            this.columnHeader11,
+            this.columnHeader12,
+            this.columnHeader13});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 397);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -69,5 +134,21 @@
 
         private Button button1;
         private Button button2;
+        private Button button3;
+        private Button button4;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader columnHeader8;
+        private ColumnHeader columnHeader9;
+        private ColumnHeader columnHeader10;
+        private ColumnHeader columnHeader11;
+        private ColumnHeader columnHeader12;
+        private ColumnHeader columnHeader13;
     }
 }
--- GistOne/Form1.cs
+++ GistOne/Form1.cs
@@ -1,5 +1,6 @@
 using GistNet;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace GistOne
 {
@@ -8,8 +9,6 @@ namespace GistOne
         public Form1()
         {
             InitializeComponent();
-
-
         }
 
         private async void button1_Click(object sender, EventArgs e)
@@ -19,32 +18,59 @@ namespace GistOne
                 string tmptoken = File.ReadAllText("token.txt");
 
                 Create TheGist = new() { Token = tmptoken };
-                TheGist.Content.@public = false;
-                TheGist.Content.description = "descrizione test, di non so quanti caratteri test";
-                TheGist.Content.files.Add("come creare un file quando non si ha nulla da fare.cs", new("public Form1()\r\n        {\r\n            InitializeComponent();\r\n\r\n\r\n        }"));
+                TheGist.Content.IsPublic = false;
+                TheGist.Content.Description = "descrizione test, di non so quanti caratteri test";
+                TheGist.Content.FilesList.Add("new file.cs", new("public Form1()\r\n        {\r\n            InitializeComponent();\r\n\r\n\r\n        }"));
                 //TheGist.Content.files.Add("Z file.txt", new("contenuto del file"));
                 //TheGist.Content.files.Add("README.md", new("# potrebbe essere la descrizione della gist"));
 
-                await TheGist.Push();
+                string jsonString = await TheGist.Push();
+                JsonNode Jobj = JsonNode.Parse(jsonString);
+
+
+                if (Jobj != null)
+                {
+                    Root jest = new()
+                    {
+                        Url = Jobj["url"].ToString(),
+                    };
+                }
+                //var jsonString22 = await jsonString.Content.ReadAsStringAsync();
+                //var jsonObject = JsonNode.Parse(jsonString22).AsObject();
+                Root test = JsonSerializer.Deserialize<Root>(jsonString);
             }
             catch (Exception ex) { MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button2_Click(object sender, EventArgs e)
         {
             try
             {
-                //string tmptoken = File.ReadAllText("token.txt");
+                string tmptoken = File.ReadAllText("token.txt");
 
-                //Create TheGist = new() { Token = tmptoken };
-                //TheGist.Content.Public = false;
-                //TheGist.Content.Description = "test";
-                //TheGist.Content.Files.Add("test.md", new("# title\n\nthis is a test"));
-                //TheGist.Content.Files.Add("file.txt", new("contenuto del file"));
-                //TheGist.Content.Files.Add("altro file.md", new("this is only a test"));
+                Browse TheGist = new() { Token = tmptoken, User = "RallyTuning" };
+                string jsonString = await TheGist.Get();
 
-                //string serializzato = JsonSerializer.Serialize(TheGist.Content);
+                if (string.IsNullOrWhiteSpace(jsonString)) { throw new Exception("Json is empty"); }
 
+                JsonNode Jobj = JsonNode.Parse(jsonString)!;
+
+                if (Jobj is null) { throw new Exception("Json is empty"); }
+
+                foreach (JsonNode? item in Jobj.AsArray())
+                {
+                    if (item is null) { return; }
+
+                    ListViewItem LvItm = new(item["description"]!.ToString()) { Tag = item["id"].ToString() };
+                    LvItm.SubItems.Add(item["public"].ToString());
+                    LvItm.SubItems.Add(item["created_at"].ToString());
+                    LvItm.SubItems.Add(item["updated_at"].ToString());
+                    LvItm.SubItems.Add(item["url"].ToString());
+                    JsonObject TmpNode = (JsonObject)item["files"];
+                    LvItm.SubItems.Add(TmpNode.Count.ToString());
+
+                    listView1.Items.Add(LvItm);
+                }
             }
             catch (Exception ex) { MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
         }
